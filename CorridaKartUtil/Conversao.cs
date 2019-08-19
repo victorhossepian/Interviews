49802bb14a01a0bcf930bbae63ce9ff8e4c7621f
victorhossepian <54288879+victorhossepian@users.noreply.github.com>
2019-08-19 16:38:26 -0300

    Add files via upload

---
+++ CorridaKartUtil/Conversao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorridaKart.Util
+{
+    public static class Conversao
+    {
+        public static TimeSpan StringTimeSpan(String valor)
+        {
+            TimeSpan tempoVolta;
+            TimeSpan.TryParseExact(valor, @"m\:ss\.fff", null, out tempoVolta);
+            return tempoVolta;
+        }
+    }
+}
